301f282dd84cade0e8827463ca1faad479913112
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-03-05 00:21:26 +0900

    ループカウンタを一つで回せるように、余分なループカウンタを削除

--- 02.calendar/cal.js
+++ 02.calendar/cal.js
@@ -16,18 +16,19 @@ process.stdout.write("      ");
 console.log(`${month}月 ${year}`);
 console.log("日 月 火 水 木 金 土");
 
-for (let i = 1; i <= lastDateOfMonth; i++) {
+for (let i = startDate; i <= lastDate; i.setDate(i.getDate() + 1)) {
   if (i.getDate() === 1) {
     process.stdout.write(indentByStartDay);
-    isFirstLineOfMonth = false;
   }
-  if (i < 10) {
+  if (i.getDate() < 10) {
     process.stdout.write(` ${String(i.getDate())}`);
   } else {
-    process.stdout.write(String(i));
+    process.stdout.write(String(i.getDate()));
   }
   if (i.getDay() !== 6 && i !== lastDate.getDate()) {
+    process.stdout.write(" ");
+  }
+  if (i.getDay() === 6) {
     console.log();
   }
-  dayOfWeekCounter++;
 }
