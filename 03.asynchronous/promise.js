d96bd0bec859fdb2665ec4b2123e6c68edd09e2b
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-04 22:30:06 +0900

    promise：エラーなし・エラーありを共通メソッドを利用するよう修正

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -2,80 +2,43 @@
 
 import sqlite3Module from "sqlite3";
 import timers from "timers/promises";
+import { runQuery, getRow, closeDb } from "./db_operation.js";
 const sqlite3 = sqlite3Module.verbose();
 const db = new sqlite3.Database(":memory:");
 
-// Promiseエラーなし
-new Promise((resolve) => {
-  db.run("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",()=>{
-    resolve();
+runQuery(
+  db,
+  "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+)
+  .then(() => {
+    return runQuery(
+      db,
+      "INSERT INTO books(title) VALUES (?)",
+      "初めてのJavaScript 第3版",
+    );
   })
-}).then(()=>{
-  return new Promise((resolve) => {
-    db.run("INSERT INTO books(title) VALUES (?)", "初めてのJavaScript 第3版", function(){
-      console.log(this.lastID);
-    })
-    resolve();
+  .then((result) => {
+    console.log(result.lastID);
   })
-}).then(()=>{
-  return new Promise((resolve) => {
-    db.get("SELECT id, title FROM books", function(error, book){
-      console.log(`${book.id} : ${book.title}`);
-    })
-    resolve();
+  .then(() => {
+    return getRow(db, "SELECT id, title FROM books");
   })
-}).then(()=>{
-    db.close();
-})
+  .then((row) => {
+    console.log(`${row.id} : ${row.title}`);
+  });
 
 await timers.setTimeout(1000);
 
-// Promiseエラーあり
-const firstPromiseFunc = () => {
-  return new Promise((resolve) => {
-  db.run("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",()=>{
-      resolve();
-    })
+runQuery(db, "INSERT INTO Book(title) VALUES (?)", "初めてのJavaScript 第3版")
+  .catch((error) => {
+    console.error(error.message);
   })
-}
-
-const secondPromiseFunc = () => {
-  return new Promise((resolve,reject) => {
-    db.run("INSERT INTO book(title) VALUES (?)", "初めてのJavaScript 第3版", function(err){
-      if (err) {
-        return reject(err);
-      } else {
-        return resolve(this.lastID);
-      }
-    })
+  .then(() => {
+    return runQuery(db, "SELECT id, title FROM Book");
   })
-}
-
-const thirdPromiseFunc = () => {
-  return new Promise((resolve,reject) => {
-    db.get("SELECT id, title FROM book", function(err, row){
-      if (err) {
-        return reject(err);
-      } else {
-        return resolve(`${row.id} : ${row.title}`);
-      }
-    })
+  .catch((error) => {
+    console.error(error.message);
   })
-}
-
-firstPromiseFunc()
-  .then(()=>{
-    return secondPromiseFunc();
-  })
-  .catch((error)=>{
-    console.error(error.message)
-  })
-  .then(()=>{
-    return thirdPromiseFunc();
-  })
-  .catch((error)=>{
-    console.error(error.message)
-  })
-  .then(()=>{
-    db.close();
-  })
+  .then(() => {
+    return closeDb();
+  });
