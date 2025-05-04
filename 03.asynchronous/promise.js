d68c7def6b4d16a6bc9f5000dd94e277e5a389a0
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-04 21:52:20 +0900

    promise：エラーありの実装（メソッド未修正）

--- 03.asynchronous/promise.js
+++ 03.asynchronous/promise.js
@@ -27,3 +27,55 @@ new Promise((resolve) => {
 }).then(()=>{
     db.close();
 })
+
+await timers.setTimeout(1000);
+
+// Promiseエラーあり
+const firstPromiseFunc = () => {
+  return new Promise((resolve) => {
+  db.run("CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",()=>{
+      resolve();
+    })
+  })
+}
+
+const secondPromiseFunc = () => {
+  return new Promise((resolve,reject) => {
+    db.run("INSERT INTO book(title) VALUES (?)", "初めてのJavaScript 第3版", function(err){
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(this.lastID);
+      }
+    })
+  })
+}
+
+const thirdPromiseFunc = () => {
+  return new Promise((resolve,reject) => {
+    db.get("SELECT id, title FROM book", function(err, row){
+      if (err) {
+        return reject(err);
+      } else {
+        return resolve(`${row.id} : ${row.title}`);
+      }
+    })
+  })
+}
+
+firstPromiseFunc()
+  .then(()=>{
+    return secondPromiseFunc();
+  })
+  .catch((error)=>{
+    console.error(error.message)
+  })
+  .then(()=>{
+    return thirdPromiseFunc();
+  })
+  .catch((error)=>{
+    console.error(error.message)
+  })
+  .then(()=>{
+    db.close();
+  })
