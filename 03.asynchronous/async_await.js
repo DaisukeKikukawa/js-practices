389e79f723fcd7d8b34630912a793d2831403e34
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2025-05-04 22:49:47 +0900

    async・await：エラーなしの実装

---
+++ 03.asynchronous/async_await.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+import sqlite3Module from "sqlite3";
+import timers from "timers/promises";
+import { runQuery, getRow, closeDb } from "./db_operation.js";
+const sqlite3 = sqlite3Module.verbose();
+const db = new sqlite3.Database(":memory:");
+
+(async () => {
+  await runQuery(
+    db,
+    "CREATE TABLE IF NOT EXISTS books(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE)",
+  );
+  const result = await runQuery(
+    db,
+    "INSERT INTO books(title) VALUES (?)",
+    "初めてのJavaScript 第3版",
+  );
+  console.log(`${result.lastID}`);
+  const row = await getRow(db, "SELECT id, title FROM books");
+  console.log(`${row.id} : ${row.title}`);
+})();
